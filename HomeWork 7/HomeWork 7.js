63bd4c3953a3c50d1e67a38b67192a2f54cd547f
Koshova Natalia <nata.nech@gmail.com>
2019-12-14 18:15:20 +0200

    task#2

--- HomeWork 7/HomeWork 7.js
+++ HomeWork 7/HomeWork 7.js
@@ -18,3 +18,33 @@ function convert(obj) {
 }
 res = convert(obj);
 console.log(res);
+///-----------------------------
+var obj1 = {
+    x: 10, 
+    y: 20
+    }
+var obj2 = {
+    z: 30
+    }
+var obj3 = {
+    x: 10
+}
+var obj4 = {
+    x: 20,
+    y: 30
+}
+var obj5 = {
+    h: 30,
+    n: 40
+}
+var obj6 = {
+    name: 'Vasya',
+    surname: 'Smirnov'
+}
+function assignObjects(ob1, ob2) {
+    var nObj = {}
+    Object.assign(nObj, ob1, ob2);
+    console.log(nObj);   
+}
+assignObjects(obj1, obj2);
+assignObjects(obj3, obj4);
