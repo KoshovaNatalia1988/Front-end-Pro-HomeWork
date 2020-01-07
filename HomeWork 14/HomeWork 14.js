7bac711a356e9295a686aa4ced7a781c1907f21a
Koshova Natalia <nata.nech@gmail.com>
2020-01-08 00:42:38 +0200

    2

--- HomeWork 14/HomeWork 14.js
+++ HomeWork 14/HomeWork 14.js
@@ -5,4 +5,8 @@ window.onload = function() {
     box.onclick = function() {
         box.classList.toggle('active');
     };
+    document.addEventListener('click', function(event) {
+        box.style.top = event.clientY + 'px';
+        box.style.left = event.clientX + 'px';
+    });
 }
