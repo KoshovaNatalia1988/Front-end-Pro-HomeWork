c9552e2b07ce0451ed82ad5274a50a598b9a894b
Koshova Natalia <nata.nech@gmail.com>
2020-01-19 17:45:15 +0200

    2

--- HomeWork 9/HomeWork-9.js
+++ HomeWork 9/HomeWork-9.js
@@ -20,3 +20,36 @@ function mul(a,b) {
     return a * b;
 };
 console.log(obj.doFunction(mul,7,8).copy('some_name'));
+//////////////////////////////////////////////////
+var str = 'var_text_hello';
+var newStr = str.split("_");
+function modification_Str(str) {
+    for (var i = 1; i < newStr.length; i++) {
+        var letterBig = newStr[i][0].toUpperCase();
+        for (var j = 1; j < newStr[i].length; j++) {
+            letterBig += newStr[i][j];
+        };
+        newStr[i] = letterBig;
+    };
+    str = "";
+    for (var i = 0; i < newStr.length; i++) {
+        str += newStr[i];
+    }
+    return str;
+};
+var result = modification_Str(newStr);
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
