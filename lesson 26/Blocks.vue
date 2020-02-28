b8bbbf418b4593450a66d9246db0056ee6b6c1fe
Koshova Natalia <nata.nech@gmail.com>
2020-02-28 14:01:06 +0200

    lesson-26

---
+++ lesson 26/Blocks.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="blocks-wrapper">
+    <div class="blocks-wrapper__block">
+        {{getName}}
+        <button @click="onClick">{{block.name}}</button>
+       
+      </div>
+  </div>
+</template>
+
+<script>
+import { mapMutations, mapActions, mapGetters} from 'vuex';
+export default {
+  name: 'Blocks',
+  props: {
+      block: Object
+  },
+  data () {
+    return {
+      
+    }
+  },
+  computed: {
+    ...mapGetters(['getName'])
+  },
+  methods: {
+    ...mapMutations(['setName']),
+    onClick () {
+       this.setName(this.block.name);
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+    .blocks-wrapper__block {
+       display: flex;
+       flex-direction: column;
+       justify-content: space-between;
+       width: 200px;
+       height:200px;
+       padding: 5px;
+       border: 2px solid red;
+       margin: 20px;
+     }
+</style>
---
+++ lesson 26/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper">
+   <Blocks v-for="blocks in blocksName" :block="blocks">
+   </Blocks>
+  </div>
+</template>
+
+<script>
+import Blocks from './Blocks';
+export default {
+  name: 'HelloWorld',
+  components: {
+    Blocks: Blocks
+  },
+  props: {
+  },
+  data () {
+    return {
+      blocksName: [
+        {name: 'Петя'},
+        {name: 'Ирина'},
+        {name: 'Коля'}
+      ]
+      
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+   .wrapper {
+     display: flex;
+     flex-direction: row;
+     justify-content: center;
+     align-content: space-between;
+   }
+</style>
